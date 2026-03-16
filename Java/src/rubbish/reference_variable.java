94cb3462c6c679ef663ab01625e203538966e864
Sangramjit Roy <sangramjit.roy.baartech.baartech@baariga.local>
2026-03-16 19:19:38 +0530

    Reference variable and constructor

--- Java/src/rubbish/reference_variable.java
+++ Java/src/rubbish/reference_variable.java
@@ -1,5 +1,11 @@
 package rubbish;
 
 public class reference_variable { //page 54
-
+    // REFERENCE VARIABLES
+        // Reference variable is a special type of variable which will be used to store two values.
+            // 1 is object address and
+            // 2 is null value
+            // Ex:- Demo a = new Demo();
+    // Constructor
+        // Constructor is a special type of method which is used to initialize the variables or datatypes.
 }
